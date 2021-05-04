4bbf8d94d337b440fcfe1d82e7504173af886e96
eze <salaseze@gmail.com>
2021-05-04 14:55:55 -0300

    Cross origin

--- src/main/java/com/example/ensolvers/controllers/FolderController.java
+++ src/main/java/com/example/ensolvers/controllers/FolderController.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class FolderController {
     @Autowired
     private FolderRepository folderRepository;
